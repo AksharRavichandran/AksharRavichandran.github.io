9e2255e6931631cfde2839606a87e5c369c17c71
Akshar Ravichandran <akshar.ravichandran@gatech.edu>
2025-09-02 10:53:21 -0400

    change entry point

--- aksharravichandran/vite.config.js
+++ aksharravichandran/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aksharravichandran/' // <-- IMPORTANT: replace with your repo name
+  base: '/aksharravichandran/'
 });
